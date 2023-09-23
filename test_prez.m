33a375cc15aeb69eba494c1785089fb857e10455
George Lyu <georgemylyu@gmail.com>
2023-09-23 09:34:04 -0500

    Testing MATLAB presentation builder

---
+++ test_prez.m
@@ -0,0 +1,23 @@
+import mlreportgen.ppt.*
+
+pptview("myPresentation.pptx",'closedoc')
+
+ppt = Presentation('myPresentation.pptx', 'myPresentation.pptx');
+open(ppt)
+
+titleSlide = add(ppt,'Title Slide');
+replace(titleSlide,'Title','Create Histogram Plots');
+close(ppt)
+pptview("myPresentation.pptx")
+
+pause(2)
+pptview("myPresentation.pptx",'closedoc')
+pause(2)
+
+ppt = Presentation('myPresentation.pptx', 'myPresentation.pptx');
+open(ppt)
+slide2 = add(ppt,'Title and Content');
+replace(slide2,'Title','Population Modeling Approach');
+disp("here")
+close(ppt)
+pptview("myPresentation.pptx")
